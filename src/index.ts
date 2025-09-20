fb0b5edf6e00c7bfbaec1d15c1c643068919a3dd
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-20 02:02:01 +0200

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from './server';
+
+const server = createServer();
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
