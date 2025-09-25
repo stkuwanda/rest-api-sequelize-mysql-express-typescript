6ea100f055c61710aaa8344c3a94eed05f707b4f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-25 20:08:39 +0200

    generate and setup standard migration, remove sequelize sync-migration at startup

---
+++ src/data/migrations/2025.09.25T17.33.22.users-table.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import type { Migration } from '../../umzug';
+
+// Migration to create the users table
+export const up: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('users', {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+    },
+  });
+}
+
+// Migration to drop the users table
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable('users');
+}
+
