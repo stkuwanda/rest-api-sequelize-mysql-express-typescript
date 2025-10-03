12130febd83d2cccbe19c1eea7614f5c8f346cf9
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-03 23:33:29 +0200

    comments on seeder file

--- seeder.js
+++ seeder.js
@@ -1,2 +1,5 @@
+// seeder.js
+// This file is used to run seeders from the command line
+// It uses ts-node to execute TypeScript code directly
 require('ts-node/register');
 require('./src/umzug').seeder.runAsCLI();
